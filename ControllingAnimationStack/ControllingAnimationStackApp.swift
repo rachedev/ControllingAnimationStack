36f48841a0f3f0617d8d5ca3b74eb42448becabe
Rachelle Lee <rachedeveloper@gmail.com>
2021-08-02 13:37:59 -0700

    Initial Commit

---
+++ ControllingAnimationStack/ControllingAnimationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControllingAnimationStackApp.swift
+//  ControllingAnimationStack
+//
+//  Created by RachelleLee on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct ControllingAnimationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
